3dffe763ee368d872b91c23c713025231ae1cd0f
derek <tillerderek@gmail.com>
2025-05-06 22:21:27 -0400

    fixed camera init to backfacing

--- static/js/circular_ui.js
+++ static/js/circular_ui.js
@@ -23,12 +23,31 @@ function drawMask() {
   ctx.globalCompositeOperation = 'source-over';
 }
 
-// Start camera
-navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-  video.srcObject = stream;
-}).catch(err => {
-  alert("Camera access denied.");
-});
+function initCamera() {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          facingMode: 'environment', // Prefer back camera
+          width: { ideal: 1280 },
+          height: { ideal: 720 },
+        },
+        audio: false,
+      })
+      .then((stream) => {
+        video.srcObject = stream;
+
+        // If you had other UI toggles like cameraScreen.classList.remove('hidden'), do them here
+      })
+      .catch((err) => {
+        console.error('Error accessing camera: ', err);
+        alert('Could not access camera. Please check permissions and try again.');
+      });
+  } else {
+    alert("Sorry, your browser doesn't support camera access");
+  }
+}
+initCamera();
 
 window.addEventListener('resize', resize);
 resize();
